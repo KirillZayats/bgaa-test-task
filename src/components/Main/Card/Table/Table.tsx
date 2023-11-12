247fbfdf70f1a9cf6e5cb1ea65c62743833f256b
Kirill <kirill.zayats.99@mail.ru>
2023-11-12 10:56:19 +0300

    refactor: adaptive style and feat: add sceleton redux

--- src/components/Main/Card/Table/Table.tsx
+++ src/components/Main/Card/Table/Table.tsx
@@ -1,18 +1,12 @@
 import "./table.scss";
 import iconPlus from "../../../../resources/plus.svg";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import iconAddAll from "../../../../resources/set-all.svg";
 import { useState } from "react";
 import Selector from "./Selector/Selector";
 
 const Table = () => {
   const teachers = ["Вакансия", "петя баночкин", "вася пупкин"];
   const [value, setValue] = useState<string | null>(teachers[0]);
-
-  //  const selectedValues = useMemo(
-  //     () => teachers.filter((v) => v.selected),
-  //     [teachers],
-  //   );
 
   return (
     <section className="table">
@@ -25,7 +19,12 @@ const Table = () => {
         </div>
         <div className="table__container table__teacher">
           <span>Преподаватель</span>
-          <img className="table__image-plus" src={iconPlus} alt="plus" />
+          <img
+            className="table__image-plus"
+            src={iconPlus}
+            alt="Картинка плюса"
+            title="Нажмите чтобы добавить новую группу"
+          />
         </div>
       </div>
       <div className="table__item table__main-item">
@@ -35,8 +34,16 @@ const Table = () => {
         <div className=" table__container table__hours">
           <span>22</span>
         </div>
-        <div className="table__container table__teacher">
+        <div className="table__container table__teacher table__teacher__set-all">
           <Selector teachers={teachers} />
+          <div className="table__container-image">
+          <img
+            className="table__image-set-all"
+            src={iconAddAll}
+            alt="Картинка применить"
+            title="Нажмите чтобы применить выбранного переподавателя "
+          />
+          </div>
         </div>
       </div>
       <div className="table__item table__main-item">
@@ -51,10 +58,10 @@ const Table = () => {
         </div>
       </div>
       <div className="table__item table__main-item">
-        <div className="table__container table__task test">
+        <div className="table__container table__task table__item__bigger-mobile">
           <span>Практические</span>
         </div>
-        <div className="table__container table__hours no-active">
+        <div className="table__container table__hours table__item__no-active-mobile">
           <span></span>
         </div>
         <div className="table__container table__teacher">
@@ -62,13 +69,13 @@ const Table = () => {
         </div>
       </div>
       <div className="table__item table__foot">
-        <div className="table__container table__task">
+        <div className="table__container table__task table__item__bigger-mobile">
           <span>Примечание</span>
-          <span className="table__task__small">
+          <span className="table__task__small ">
             (для составления расписания)
           </span>
         </div>
-        <div className="table__container table__hours">
+        <div className="table__container table__hours table__item__no-active-mobile">
           <span></span>
         </div>
         <div className="table__container table__teacher">
---
+++ src/store/types/types.ts
@@ -0,0 +1,15 @@
+export enum DataActionType {
+    GET_DATA = "GET_DATA",
+    POST_DATA = "POST_DATA",
+  }
+
+interface IGetDataAction {
+
+}
+
+interface IGetDataAction {
+    
+}
+
+
+  export type DataAction = IGetDataAction | IGetDataAction;
